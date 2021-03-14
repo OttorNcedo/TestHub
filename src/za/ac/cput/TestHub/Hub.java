b5f4614b648adf4efd08b70795f7209aeadbb9a6
Bukhona Ncedo <bukhona974@gmail.com>
2021-03-15 00:24:35 +0200

    This is the third commit made by @Ottor  Ncedo

--- src/za/ac/cput/TestHub/Hub.java
+++ src/za/ac/cput/TestHub/Hub.java
@@ -7,6 +7,7 @@ package za.ac.cput.TestHub;
 public class Hub {
     /**
      * Returns a result of integer division
+     * This was edited by @Bukhona Ncedo
      * @param numerator
      * @param denominator
      * @return
@@ -14,8 +15,8 @@ public class Hub {
     public static int IntegerDivision (int numerator, int denominator){
         if (denominator <1 ) return 0;
         return numerator / denominator;
-
     }
+
 
 
 }
